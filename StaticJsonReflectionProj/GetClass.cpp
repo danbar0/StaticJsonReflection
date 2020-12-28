745ca41a4744b1451adb17d3da61614876b2a360
danbar0 <35388915+danbar0@users.noreply.github.com>
2020-12-28 15:25:38 -0600

    Added enumType to Type struct and switch case in SerializeObject for different types

--- StaticJsonReflectionProj/GetClass.cpp
+++ StaticJsonReflectionProj/GetClass.cpp
@@ -1,9 +1,6 @@
 #include "pch.h"
 #include "GetClass.hpp"
 
-DEFINE_TYPE(uint8_t)
 DEFINE_TYPE(int8_t)
-DEFINE_TYPE(uint16_t)
 DEFINE_TYPE(int16_t)
-DEFINE_TYPE(uint32_t)
 DEFINE_TYPE(int32_t)
--- StaticJsonReflectionProj/GetClass.hpp
+++ StaticJsonReflectionProj/GetClass.hpp
@@ -4,9 +4,16 @@
 
 #define MAX_NUMBER_OF_FIELDS 10
 
+enum class TypeName {
+		int8_t,
+		int16_t,
+		int32_t
+};
+
 struct Type {
-		std::string name; 
-		size_t size; 
+		std::string stringName; 
+		TypeName enumName; 
+		size_t size;
 };
 
 struct Field {
@@ -29,8 +36,9 @@ const Class* GetClass();
 template<> \
 const Type* GetType<TYPE>() { \
 		static Type type; \
-		type.name = #TYPE; \
+		type.stringName = #TYPE; \
 		type.size = sizeof(TYPE); \
+		type.enumName = TypeName::TYPE; \
 		return &type; \
 }\
 
